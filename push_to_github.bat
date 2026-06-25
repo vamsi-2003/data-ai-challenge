e9e3ec0a0e3ca13f5d501705579d541dc5126751
vamsi-2003 <151930171+vamsi-2003@users.noreply.github.com>
2026-06-25 11:30:02 +0530

    Initial commit of candidate ranking system

--- push_to_github.bat
+++ push_to_github.bat
@@ -8,11 +8,12 @@ git commit -m "Initial commit of candidate ranking system"
 git branch -M main
 
 echo [3/4] Adding GitHub remote...
+# Remove origin if it already exists, then add
+git remote remove origin 2>nul
 git remote add origin https://github.com/vamsi-2003/data-ai-challenge.git
 
-echo [4/4] Pushing to GitHub...
-echo Please make sure you have created a public repository named "data-ai-challenge" at https://github.com/vamsi-2003
-git push -u origin main
+echo [4/4] Pushing to GitHub (Force Overwriting remote defaults)...
+git push -u origin main --force
 
 echo Push complete!
 pause
